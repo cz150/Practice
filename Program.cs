a3b0caaf8c1d6913f1115ccaae409b21fc884fa7
fanzhibo <2067761463@qq.com>
2021-08-06 19:10:38 +0800

    lock锁

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Practice
 {
@@ -6,7 +7,51 @@ namespace Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Thread[] threads = new Thread[10000];
+            Program acc = new Program(10);
+            for (int i = 0; i < threads.Length; i++)
+            {
+                Thread t = new Thread(new ThreadStart(acc.DoTransactions));
+                threads[i] = t;
+            }
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i].Start();
+            }
+
+        }
+        int balance;
+
+        Random r = new Random();
+
+        public Program(int initial)
+        {
+            balance = initial;
+        }
+
+        void Withdraw(int amount)
+        {
+            lock (this)
+            {
+                if (balance == 0)
+                {
+                    Console.WriteLine("以抢完！");
+                    throw new Exception("以抢完!");
+                }
+                Console.WriteLine("抢之前:  " + balance);
+                Console.WriteLine("抢到数据: -" + amount);
+                balance = balance - amount;
+                Console.WriteLine("抢之后:  " + balance);
+                Console.WriteLine();
+            }
+        }
+
+        public void DoTransactions()
+        { 
+            for (int i = 0; i < 10; i++)
+            {
+                Withdraw(1);
+            }
         }
     }
 }
