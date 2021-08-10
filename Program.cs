e37ce8760af4791521bc81c9803c809e92a28871
look up <2067761463@qq.com>
2021-08-10 15:38:47 +0800

    事务

--- Program.cs
+++ Program.cs
@@ -23,26 +23,28 @@ namespace Practice
             //}
             #endregion
             #region AsyncLock 异步锁
-            
-            Program acc = new Program(100);//通过构造函数给变量赋值，相当于100红包
-            Thread[] threads = new Thread[100];//创建100个线程模拟100人抢100元红包
-            for (int i = 0; i < threads.Length; i++)
-            {
-                Thread t = new Thread(() =>
-                {
-                    DoTransactions("线程" + i + ":").Wait();
-                });
-                threads[i] = t;
-            }
-            for (int i = 0; i < threads.Length; i++)//开启线程
-            {
-                threads[i].Start();
-            }
-            Console.Read();
+            //Program acc = new Program(100);//通过构造函数给变量赋值，相当于100红包
+            //Thread[] threads = new Thread[100];//创建100个线程模拟100人抢100元红包
+            //for (int i = 0; i < threads.Length; i++)
+            //{
+            //    Thread t = new Thread(() =>
+            //    {
+            //        DoTransactions("线程" + i + ":").Wait();
+            //    });
+            //    threads[i] = t;
+            //}
+            //for (int i = 0; i < threads.Length; i++)//开启线程
+            //{
+            //    threads[i].Start();
+            //}
             #endregion
-
+            #region 事务
+            Method1();
+            Method2();
+            #endregion
+            Console.Read();
         }
-        #region lock
+        #region lock 同步锁
         //int balance;
         //Random r = new Random();
         //public Program(int initial)
@@ -80,46 +82,71 @@ namespace Practice
         //}
         #endregion
         #region AsyncLock 异步锁
-        static int balance;
+        //static int balance;
 
-        public Program(int initial)
-        {
-            balance = initial;
-        }
-        public static AsyncLock asyncLock = new AsyncLock();
-        public static async Task<string> Withdraw(string name, int amount)
-        {
-            using (await asyncLock.LockAsync())
-            {
-                if (balance <= 0)
-                {
-                    Console.WriteLine(name + "没有抢到红包已经抢完！");
-                }
-                else
-                {
-                    if (balance< amount)
-                    {
-                        amount = balance;
-                        balance = 0;
-                        Console.WriteLine(name + "抢到" + amount + "元，红包剩余:" + balance);
-                    }
-                    else
-                    {
-                        balance = balance - amount;
-                        Console.WriteLine(name + "抢到" + amount + "元，红包剩余:" + balance);
-                    }
+        //public Program(int initial)
+        //{
+        //    balance = initial;
+        //}
+        //public static AsyncLock asyncLock = new AsyncLock();
+        //public static async Task<string> Withdraw(string name, int amount)
+        //{
+        //    using (await asyncLock.LockAsync())
+        //    {
+        //        if (balance <= 0)
+        //        {
+        //            Console.WriteLine(name + "没有抢到红包已经抢完！");
+        //        }
+        //        else
+        //        {
+        //            if (balance< amount)
+        //            {
+        //                amount = balance;
+        //                balance = 0;
+        //                Console.WriteLine(name + "抢到" + amount + "元，红包剩余:" + balance);
+        //            }
+        //            else
+        //            {
+        //                balance = balance - amount;
+        //                Console.WriteLine(name + "抢到" + amount + "元，红包剩余:" + balance);
+        //            }
                     
-                }
-                return  "";
-           }
+        //        }
+        //        return  "";
+        //   }
+
+        //}
+        //public static async Task<string> DoTransactions(string name)
+        //{
+        //    Random r = new Random();
+        //    int a = r.Next(1, 10);
+        //    await Withdraw(name, a);
+        //    return "";
+        //}
+        #endregion
+        #region 事务
+        /// <summary>
+        /// 这个Attribute就是使用时候的验证，把它添加到需要执行事务的方法中，即可完成事务的操作。
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Method, Inherited = true)]
+        public class UseTranAttribute : Attribute
+        {
 
         }
-        public static async Task<string> DoTransactions(string name)
+
+        
+        public static int Method1()
         {
-            Random r = new Random();
-            int a = r.Next(1, 10);
-            await Withdraw(name, a);
-            return "";
+            //写个新增
+            return 0;
+           
+        }
+        [UseTran]
+        public static int Method2()
+        {
+            //写个新增
+            return 0;
+            throw new Exception("出现异常");
         }
         #endregion
     }
